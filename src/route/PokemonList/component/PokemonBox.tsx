c7418bc2b76c6245c65a52aa669c0951ff0edac3
nurrizkiamalia <rizkiamel9@gmail.com>
2024-05-01 21:07:31 +0700

    fix: minor change

--- src/route/PokemonList/component/PokemonBox.tsx
+++ src/route/PokemonList/component/PokemonBox.tsx
@@ -1,17 +1,17 @@
-import { useCallback } from "react";
-import usePokemonDetails from "../../../hooks/usePokemonDetail";
-import { useNavigate } from "react-router-dom";
+import { useCallback } from "react"
+import usePokemonDetails from "../../../hooks/usePokemonDetail"
+import { useNavigate } from "react-router-dom"
 
 interface CardProps {
   name: string
 }
 
 const PokemonBox: React.FC<CardProps> = ({ name }) => {
-  const { pokemonDetails, loading, error } = usePokemonDetails(name);
-  const navigate = useNavigate();
+  const { pokemonDetails, loading, error } = usePokemonDetails(name)
+  const navigate = useNavigate()
 
   const handleClickCard = useCallback(() => {
-    navigate(`/PokemonDetail/${name}`);
+    navigate(`/PokemonDetail/${name}`)
   }, [name, navigate])
 
   if (loading || !pokemonDetails) return <div>Loading...</div>
@@ -19,7 +19,7 @@ const PokemonBox: React.FC<CardProps> = ({ name }) => {
   
   return (
     <div onClick={handleClickCard} className="bg-lightBlue rounded-xl flex flex-col items-center justify-center p-5  cursor-alias shadow-md hover:scale-105 ">
-        <img src={`${pokemonDetails.artworkFront}`} alt="Pokemon" className="hover:scale-105 bg-transparent hover:bg-blueDonker hover:shadow-xl hover:shadow-darkBlue" />
+        <img src={`${pokemonDetails.artworkFront}`} alt="Pokemon" className="hover:scale-105 bg-transparent hover:from-blueDonker hover:to-lightBlue hover:shadow-xl hover:shadow-darkBlue hover:rounded-xl transition-all" />
         <h3 className="line-clamp-2 capitalize text-center font-semibold">{pokemonDetails.name}</h3>
     </div>
   )
