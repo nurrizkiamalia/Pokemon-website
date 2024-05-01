9f7bd913accc1a840762b910b4605429dd3dad97
nurrizkiamalia <rizkiamel9@gmail.com>
2024-05-01 21:08:42 +0700

    fix: minor change

--- src/route/PokemonList/component/PokemonBox.tsx
+++ src/route/PokemonList/component/PokemonBox.tsx
@@ -18,7 +18,7 @@ const PokemonBox: React.FC<CardProps> = ({ name }) => {
   if (error) return <div>Something is wrong</div>
   
   return (
-    <div onClick={handleClickCard} className="bg-lightBlue rounded-xl flex flex-col items-center justify-center p-5  cursor-alias shadow-md hover:scale-105 ">
+    <div onClick={handleClickCard} className="bg-lightBlue rounded-xl flex flex-col items-center justify-center p-5  cursor-alias shadow-lg shadow-blueDonker hover:scale-105 transition-all ">
         <img src={`${pokemonDetails.artworkFront}`} alt="Pokemon" className="hover:scale-105 bg-transparent hover:from-blueDonker hover:to-lightBlue hover:shadow-xl hover:shadow-darkBlue hover:rounded-xl transition-all" />
         <h3 className="line-clamp-2 capitalize text-center font-semibold">{pokemonDetails.name}</h3>
     </div>
